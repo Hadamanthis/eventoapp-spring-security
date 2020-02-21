d239d91753d6ed80d25839f91dc6276ce68a569a
Geovane Menezes <geovane.menezes@seati.ma.gov.br>
2020-02-21 13:28:11 -0300

    Adicionando autenticação de usuário

---
+++ src/main/java/com/eventoapp/models/Usuario.java
@@ -0,0 +1,79 @@
+package com.eventoapp.models;
+
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+public class Usuario implements UserDetails {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private String login;
+	
+	private String password;
+	
+	private String nomeCompleto;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		return login;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	// Getters and Setters
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
